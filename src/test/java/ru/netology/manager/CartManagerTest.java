0fbff63b3ffcdd1a2770798a2e207de68e348aff
Kristina <kris2015.lebedeva@yandex.ru>
2020-07-20 01:35:41 +0300

    shouldRemoveIfExist test added

--- src/test/java/ru/netology/manager/CartManagerTest.java
+++ src/test/java/ru/netology/manager/CartManagerTest.java
@@ -17,5 +17,23 @@ class CartManagerTest {
         assertArrayEquals(expected, manager.getItems());
     }
 
+    @Test
+    void shouldRemoveIfExist() {
+        int idToRemove = 1;
+        PurchaseItem first = new PurchaseItem(1, 1, "first", 1, 1);
+        PurchaseItem second = new PurchaseItem(2, 2, "second", 2, 2);
+        PurchaseItem third = new PurchaseItem(3, 3, "third", 3, 3);
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+
+        manager.removeById(idToRemove);
+
+        PurchaseItem[] actual = manager.getAll();
+        PurchaseItem[] expected = new PurchaseItem[]{third, second};
+
+        assertArrayEquals(expected, actual);
+    }
+
 
 }
