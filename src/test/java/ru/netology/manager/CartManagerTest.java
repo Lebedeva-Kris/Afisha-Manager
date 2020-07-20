ae2670b6caf401d4f9adf4286ed08fca3ffd6dd2
Kristina <kris2015.lebedeva@yandex.ru>
2020-07-20 15:07:07 +0300

    shouldRemoveIfNotExist test added

--- src/test/java/ru/netology/manager/CartManagerTest.java
+++ src/test/java/ru/netology/manager/CartManagerTest.java
@@ -2,11 +2,15 @@ package ru.netology.manager;
 
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.PurchaseItem;
+import ru.netology.manager.CartManager;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CartManagerTest {
     CartManager manager = new CartManager();
+    PurchaseItem first = new PurchaseItem(1, 1, "first", 1, 1);
+    PurchaseItem second = new PurchaseItem(2, 2, "second", 2, 2);
+    PurchaseItem third = new PurchaseItem(3, 3, "third", 3, 3);
 
     @Test
     void shouldAdd() {
@@ -20,9 +24,21 @@ class CartManagerTest {
     @Test
     void shouldRemoveIfExist() {
         int idToRemove = 1;
-        PurchaseItem first = new PurchaseItem(1, 1, "first", 1, 1);
-        PurchaseItem second = new PurchaseItem(2, 2, "second", 2, 2);
-        PurchaseItem third = new PurchaseItem(3, 3, "third", 3, 3);
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+
+        manager.removeById(idToRemove);
+
+        PurchaseItem[] actual = manager.getAll();
+        PurchaseItem[] expected = new PurchaseItem[]{third, second};
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldRemoveIfNotExist() {
+        int idToRemove = 4;
         manager.add(first);
         manager.add(second);
         manager.add(third);
